71ef559233bdd79943b5435282230a45a5d06169
Adrián Gómez Anríquez <adrian.gomez@hotmail.cl>
2024-02-03 22:53:36 -0300

    se modifica lista y se optimiza

--- App.js
+++ App.js
@@ -1,27 +1,90 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, Pressable, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, FlatList, Pressable } from 'react-native';
 import cartLogo from './assets/cart.png'
+import jcjLogo from './assets/JC_Jenson_Logo.png'
+
+const DATA = [
+  {
+    id: 1,
+    name : "Lápiz JCJenson",
+  },
+  {
+    id: 2,
+    name : "Servicio de transporte",
+  },
+  {
+    id: 3,
+    name : "Colonias espaciales",
+  },
+  {
+    id: 4,
+    name : "Dron especialista en niños",
+  },
+  {
+    id: 5,
+    name : "Dron de reparaciones",
+  },
+  {
+    id: 6,
+    name : "Dron de servidumbre",
+  },
+  {
+    id: 7,
+    name : "Dron sin entrenamiento",
+  },
+  {
+    id: 8,
+    name : "Dron de seguridad",
+  },
+  {
+    id: 9,
+    name : "Centinela",
+  },
+
+]
 
 export default function App() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState(DATA);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItem = () => {
+    let maxId = 0;
+    for(let i = 0; i<items.length; i++){
+      maxId = items[i].id;
+    }
+    const newItem = {
+      name : inputValue,
+      id: maxId+1
+    }
+    setItems([...items, newItem])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>JCJenson</Text>
+        <Image source={jcjLogo} /> 
         <Image style={{width: 50, height: 50}} source={cartLogo} /> 
       </View>
       <View style={{flexDirection: 'row'}}>
-        <TextInput placeholder = 'Filtrar por categoria' style={styles.filtro}/>
-        <TouchableOpacity>
-          <Text style={{fontSize: 13}}>Filtrar</Text>
-        </TouchableOpacity>
+        <TextInput placeholder = 'Ingrese un nuevo producto' style={styles.filtro}
+        value={inputValue} onChangeText={setInputValue}/>
+        <Pressable onPress={addItem}>
+          <Text style={{fontSize: 30}}>+</Text>
+        </Pressable>
       </View>
-      <View style={styles.itemList}>
-        <Text style={styles.products}>Articulos de oficina</Text>
-        <Text style={styles.products}>Transporte</Text>
-        <Text style={styles.products}>Servicios Espaciales</Text>
-        <Text style={styles.products}>Drones de oficina</Text>
-        <Text style={styles.products}>Drones para el hogar</Text>
-        <Text style={styles.products}>Drones para el servicio de Seguridad</Text>
+      <View>
+        <FlatList 
+          data={items}
+          renderItem={({ item }) => (
+            <View style={styles.itemList}>
+              <Text style={styles.products}>{item.name}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.id}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
