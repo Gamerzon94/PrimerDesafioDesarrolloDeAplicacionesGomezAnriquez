f7f61aaa1c643476fb1792d9a6b55cdb5c8d486d
Adrián Gómez Anríquez <adrian.gomez@hotmail.cl>
2024-04-06 18:10:17 -0300

    Se agrega SQLite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Pressable } from "react-native";
 import jcjLogo from '../../assets/JC_Jenson_Logo.png';
 import cartLogo from '../../assets/cart.png';
 import { colors } from '../global/colors';
 import Constants from 'expo-constants';
+import { MaterialIcons } from "@expo/vector-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/auth/authSlice";
+import { deleteSession } from "../db";
 
 function Header ({title}) {
+    const { localId, user } = useSelector((state) => state.authReducer.value);
+    const dispatch = useDispatch();
+
+    const onLogout = async () => {
+        dispatch(logout());
+        const deletedSession = await deleteSession({ localId });
+    };
     return (
         <View style={styles.header}>
             <Text style = {{fontSize : 20}}>{title}</Text>
             <Image source={jcjLogo} /> 
+            {user && (
+                <Pressable style={styles.logoutIcon} onPress={onLogout}>
+                <MaterialIcons name="logout" size={24} color="white" />
+                </Pressable>
+            )}
         </View>
     );
 }
@@ -23,5 +39,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: colors.header,
         paddingTop: Constants.statusBarHeight,
+    },
+    logoutIcon: {
+        position: "absolute",
+        right: 20,
     },
 });
